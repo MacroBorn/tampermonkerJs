2e4d011e970a50212d90b2e0092b46c6e987eff2
Born <754765272@qq.com>
2019-10-03 06:13:22 +0800

    add js

---
+++ es6.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name                ES6
+// @namespace    	    http://tampermonkey.net/
+// @version      		1.0
+// @description  	    try to take over the world!
+// @author       		Born
+// @icon 		  		https://s2.ax1x.com/2019/09/26/uu9x9x.png
+// @include		   		*://*.imooc.com*
+// @require     		https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
+// @require      		https://cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.min.js
+// @grant        		GM_setValue
+// @grant        		GM_getValue
+// @grant        		GM_addStyle
+// @grant 			    unsafeWindow
+// @grant        		GM_listValues
+// @grant        		GM_deleteValue
+// @grant        		GM_xmlhttpRequest
+// @grant 				GM_getResourceText
+// @grant 			    GM_registerMenuCommand
+// @grant 			    GM_unregisterMenuCommand
+// @compatible	     	Chrome >=70
+// @license      	    MIT
+// @noframes
+// ==/UserScript==
+
+(function() {
+    "use strict";
+    var $ = $ || window.$;
+})();
